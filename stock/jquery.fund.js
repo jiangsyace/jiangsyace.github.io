651b1736b36587f391e849475ddb0d5360a45142
jiangsy <qian_962464@163.com>
2018-05-08 18:25:43 +0800

    20180508

--- stock/jquery.fund.js
+++ stock/jquery.fund.js
@@ -1,10 +1,10 @@
 /*!
  * Date: 2018-05-04
  */
-(function ( document, jquery ) {
+(function ($) {
 
 	"use strict";
-	$.extend($.fn, {
+	$.fn.extend({
 		fund: function (options) {
 
 			// if nothing is selected, return nothing; can't chain anyway
@@ -19,7 +19,7 @@
 				return fund;
 			}
 
-			fund = new $.fund(options, this[0]);
+			fund = new $.funder(options, this[0]);
 			$.data(this[0], "fund", fund);
 
 			return fund;
@@ -40,9 +40,118 @@
 				validator.errorList = errorList;
 			}
 			return valid;
-		},
-		build: function () {
 		}
-
 	});
-})(document, jquery);
+	// constructor for validator
+	$.funder = function (options, fund) {
+		this.settings = $.extend(true, {}, $.funder.defaults, options);
+		this.currentFund = fund;
+		this.init();
+	};
+
+	$.extend($.funder, {
+
+		defaults: {
+			messages: {},
+			groups: {},
+			rules: {},
+			errorClass: "error",
+			validClass: "valid",
+			errorElement: "label",
+			focusCleanup: false,
+			focusInvalid: true,
+			errorContainer: $([]),
+			errorLabelContainer: $([]),
+			onsubmit: true,
+			ignore: ":hidden",
+			ignoreTitle: false,
+			onfocusin: function (element) {
+				this.lastActive = element;
+
+				// Hide error label and remove error class on focus if enabled
+				if (this.settings.focusCleanup) {
+					if (this.settings.unhighlight) {
+						this.settings.unhighlight.call(this, element, this.settings.errorClass, this.settings.validClass);
+					}
+					this.hideThese(this.errorsFor(element));
+				}
+			},
+			onclick: function (element) {
+				// click on selects, radiobuttons and checkboxes
+				if (element.name in this.submitted) {
+					this.element(element);
+
+					// or option elements, check parent select in that case
+				} else if (element.parentNode.name in this.submitted) {
+					this.element(element.parentNode);
+				}
+			}
+		},
+		setDefaults: function (settings) {
+			$.extend($.validator.defaults, settings);
+		},
+		prototype: {
+
+			init: function () {
+				console.info('fund is init');
+				this.labelContainer = $(this.settings.errorLabelContainer);
+				this.submitted = {};
+				this.valueCache = {};
+				this.pendingRequest = 0;
+				this.pending = {};
+				this.invalid = {};
+				this.reset();
+
+				var groups = (this.groups = {}),
+					rules;
+				$.each(this.settings.groups, function (key, value) {
+					if (typeof value === "string") {
+						value = value.split(/\s/);
+					}
+					$.each(value, function (index, name) {
+						groups[name] = key;
+					});
+				});
+				rules = this.settings.rules;
+				$.each(rules, function (key, value) {
+					rules[key] = $.validator.normalizeRule(value);
+				});
+
+				function delegate(event) {
+					var validator = $.data(this.form, "validator"),
+						eventType = "on" + event.type.replace(/^validate/, ""),
+						settings = validator.settings;
+					if (settings[eventType] && !$(this).is(settings.ignore)) {
+						settings[eventType].call(validator, this, event);
+					}
+				}
+
+				$(this.currentFund)
+					.on("focusin.validate focusout.validate keyup.validate",
+						":text, [type='password'], [type='file'], select, textarea, [type='number'], [type='search'], " +
+						"[type='tel'], [type='url'], [type='email'], [type='datetime'], [type='date'], [type='month'], " +
+						"[type='week'], [type='time'], [type='datetime-local'], [type='range'], [type='color'], " +
+						"[type='radio'], [type='checkbox']", delegate)
+					// Support: Chrome, oldIE
+					// "select" is provided as event.target when clicking a option
+					.on("click.validate", "select, option, [type='radio'], [type='checkbox']", delegate);
+
+				if (this.settings.invalidHandler) {
+					$(this.currentForm).on("invalid-form.validate", this.settings.invalidHandler);
+				}
+
+				// Add aria-required to any Static/Data/Class required fields before first validation
+				// Screen readers require this attribute to be present before the initial submission http://www.w3.org/TR/WCAG-TECHS/ARIA2.html
+				$(this.currentForm).find("[required], [data-rule-required], .required").attr("aria-required", "true");
+			},
+			reset: function () {
+				this.successList = [];
+				this.errorList = [];
+				this.errorMap = {};
+				this.toShow = $([]);
+				this.toHide = $([]);
+				this.currentElements = $([]);
+			},
+		}
+	});
+})(jQuery);
--- stock/jquery.stock.js
+++ stock/jquery.stock.js
@@ -19,7 +19,7 @@
                 return fund;
             }
 
-            fund = new $.fund(options, this[0]);
+            funder = new $.funder(options, this[0]);
             $.data(this[0], "fund", fund);
 
             return fund;
@@ -42,5 +42,57 @@
             return valid;
         },
 
+    });
+
+    // constructor for validator
+    $.funder = function (options, form) {
+        this.settings = $.extend(true, {}, $.funder.defaults, options);
+        this.currentForm = form;
+        this.init();
+    };
+
+    $.extend($.funder, {
+
+        defaults: {
+            messages: {},
+            errorClass: "error",
+            validClass: "valid",
+            errorElement: "label",
+            focusCleanup: false,
+            errorContainer: $([]),
+            errorLabelContainer: $([]),
+            onsubmit: true,
+            ignore: ":hidden",
+            ignoreTitle: false,
+            onfocusin: function (element) {
+                this.lastActive = element;
+
+                // Hide error label and remove error class on focus if enabled
+                if (this.settings.focusCleanup) {
+                    if (this.settings.unhighlight) {
+                        this.settings.unhighlight.call(this, element, this.settings.errorClass, this.settings.validClass);
+                    }
+                    this.hideThese(this.errorsFor(element));
+                }
+            },
+            onfocusout: function (element) {
+                if (!this.checkable(element) && (element.name in this.submitted || !this.optional(element))) {
+                    this.element(element);
+                }
+            },
+            onclick: function (element) {
+                // click on selects, radiobuttons and checkboxes
+                if (element.name in this.submitted) {
+                    this.element(element);
+
+                    // or option elements, check parent select in that case
+                } else if (element.parentNode.name in this.submitted) {
+                    this.element(element.parentNode);
+                }
+            }
+        },
+        setDefaults: function (settings) {
+            $.extend($.validator.defaults, settings);
+        }
     });
 })(document, jquery);
